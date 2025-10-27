631545398ffbc99f8a273f46834aeeac5244a51c
Dagmer <paul.hofbauer3@gmail.com>
2025-10-27 22:12:30 +0100

    schei� encoding

--- dataloaders/csv.py
+++ dataloaders/csv.py
@@ -3,6 +3,7 @@ from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.utils import format_dataframe
 import pandas as pd
 from dataloaders.base import BaseEventLogDataset, make_feature_fn
+from charset_normalizer import from_path
 
 
 class CSVEventLogDataset(BaseEventLogDataset):
@@ -15,8 +16,12 @@ class CSVEventLogDataset(BaseEventLogDataset):
         sep=",",
         **_,
     ):
+        # determine encoding
+        result = from_path(source_path).best()
+        encoding = result.encoding or 'utf-8-sig'
         # read CSV
-        df = pd.read_csv(source_path, sep=sep, index_col=False)
+        with open(source_path, encoding=encoding, errors='replace') as f:
+            df = pd.read_csv(f, sep=sep, index_col=False)
         # standardise columns via format_dataframe (or manually rename)
         df = format_dataframe(
             df,
@@ -24,6 +29,9 @@ class CSVEventLogDataset(BaseEventLogDataset):
             activity_key=activity_col,
             timestamp_key=timestamp_col,
         )
+        self.case_id_col = case_id_col
+        self.activity_col = activity_col
+        self.timestamp_col = timestamp_col
         # convert timestamp column to datetime (if not already)
         df[timestamp_col] = pd.to_datetime(df[timestamp_col])
         # convert to EventLog
@@ -36,14 +44,19 @@ class CSVEventLogDataset(BaseEventLogDataset):
 # Example usage
 if __name__ == "__main__":
     path = "data/dx.doi.org_10.4121_uuid_c3f3ba2d-e81e-4274-87c7-882fa1dbab0d/BPI2016_Werkmap_Messages.csv"
+    path = "data/dx.doi.org_10.4121_uuid_e30ba0c8-0039-4835-a493-6e3aa2301d3f/BPI2016_Complaints.csv"
+    # path = "data/dx.doi.org_10.4121_uuid_9b99a146-51b5-48df-aa70-288a76c82ec4/BPI2016_Clicks_NOT_Logged_In.csv"
+
+    from dataloaders.util import CONSTRUCTION_PARAMS
+
+    params = CONSTRUCTION_PARAMS[
+        "dx.doi.org_10.4121_uuid_e30ba0c8-0039-4835-a493-6e3aa2301d3f"
+    ]
+
     dataset = CSVEventLogDataset(
         path,
-        # attribute="EventType",
-        case_id_col="CustomerID",
-        timestamp_col="EventDateTime",
-        activity_col="HandlingChannelID",
-        sep=";",
         feature_fn=make_feature_fn,
+        **params,
     )
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=4, collate_fn=dataset.collate_fn
--- dataloaders/util.py
+++ dataloaders/util.py
@@ -35,3 +35,88 @@ def _normalize_log_input(subset):
         )
 
     raise TypeError(f"Unsupported subset type: {type(subset)}")
+
+
+CONSTRUCTION_PARAMS = {
+    # HEADER:
+    # CustomerID;AgeCategory;Gender;Office_U;Office_W;ContactDate;ContactTimeStart;ContactTimeEnd;QuestionThemeID;QuestionSubthemeID;QuestionTopicID;QuestionTheme;QuestionSubtheme;QuestionTopic;QuestionTheme_EN;QuestionSubtheme_EN;QuestionTopic_EN
+    "dx.doi.org_10.4121_uuid_2b02709f-9a84-4538-a76a-eb002eacf8d1": {
+        "rtype": "csv",
+        "case_id_col": "CustomerID",
+        "timestamp_col": "ContactTimeStart",
+        "activity_col": "QuestionTopic_EN",
+        "sep": ";",
+    },
+    # HEADER:
+    # CI Name (aff);CI Type (aff);CI Subtype (aff);Service Component WBS (aff);Incident ID;Status;Impact;Urgency;Priority;Category;KM number;Alert Status;# Reassignments;Open Time;Reopen Time;Resolved Time;Close Time;Handle Time (Hours);Closure Code;# Related Interactions;Related Interaction;# Related Incidents;# Related Changes;Related Change;CI Name (CBy);CI Type (CBy);CI Subtype (CBy);ServiceComp WBS (CBy)
+    "dx.doi.org_10.4121_uuid_3cfa2260-f5c5-44be-afe1-b70d35288d6d": {
+        "rtype": "csv",
+        "case_id_col": "Incident ID",
+        "timestamp_col": "Open Time",
+        "activity_col": "CI Name (aff)",
+        "sep": ";",
+    },
+    # HEADER:
+    # CI Name (aff);CI Type (aff);CI Subtype (aff);Service Comp WBS (aff);Interaction ID;Status;Impact;Urgency;Priority;Category;KM number;Open Time (First Touch);Close Time;Closure Code;First Call Resolution;Handle Time (secs);Related Incident
+    "dx.doi.org_10.4121_uuid_3d5ae0ce-198c-4b5c-b0f9-60d3035d07bf": {
+        "rtype": "csv",
+        "case_id_col": "Interaction ID",
+        "timestamp_col": "Open Time (First Touch)",
+        "activity_col": "Category",  # ?
+        "sep": ";",
+    },
+    # HEADER:
+    # SessionID;IPID;TIMESTAMP;VHOST;URL_FILE;PAGE_NAME;REF_URL_category;page_load_error;page_action_detail;tip;service_detail;xps_info;page_action_detail_EN;service_detail_EN;tip_EN
+    "dx.doi.org_10.4121_uuid_9b99a146-51b5-48df-aa70-288a76c82ec4": {
+        "rtype": "csv",
+        "case_id_col": "SessionID",
+        "timestamp_col": "TIMESTAMP",
+        "activity_col": "page_action_detail_EN",
+        "sep": ";",
+    },
+    # HEADER:
+    # CustomerID;AgeCategory;Gender;Office_U;Office_W;SessionID;IPID;TIMESTAMP;VHOST;URL_FILE;PAGE_NAME;REF_URL_category;page_load_error;page_action_detail;tip;service_detail;xps_info;page_action_detail_EN;service_detail_EN;tip_EN
+    "dx.doi.org_10.4121_uuid_01345ac4-7d1d-426e-92b8-24933a079412": {
+        "rtype": "csv",
+        "case_id_col": "CustomerID",
+        "timestamp_col": "TIMESTAMP",
+        "activity_col": "page_action_detail_EN",
+        "sep": ";",
+    },
+    # HEADER:
+    # Incident ID;DateStamp;IncidentActivity_Number;IncidentActivity_Type;Assignment Group;KM number;Interaction ID
+    "dx.doi.org_10.4121_uuid_86977bac-f874-49cf-8337-80f26bf5d2ef": {
+        "rtype": "csv",
+        "case_id_col": "Incident ID",
+        "timestamp_col": "DateStamp",
+        "activity_col": "IncidentActivity_Type",
+        "sep": ";",
+    },
+    # HEADER:
+    # CustomerID;AgeCategory;Gender;Office_U;Office_W;EventDateTime;EventType;HandlingChannelID
+    "dx.doi.org_10.4121_uuid_c3f3ba2d-e81e-4274-87c7-882fa1dbab0d": {
+        "rtype": "csv",
+        "case_id_col": "CustomerID",
+        "timestamp_col": "EventDateTime",
+        "activity_col": "EventType",
+        "sep": ";",
+    },
+    # HEADER:
+    # CustomerID;AgeCategory;Gender;Office_U;Office_W;ComplaintDossierID;ComplaintID;ContactDate;ContactChannelID;ComplaintThemeID;ComplaintSubthemeID;ComplaintTopicID;ComplaintTheme;ComplaintSubtheme;ComplaintTopic;ComplaintTheme_EN;ComplaintSubtheme_EN;ComplaintTopic_EN
+    "dx.doi.org_10.4121_uuid_e30ba0c8-0039-4835-a493-6e3aa2301d3f": {
+        "rtype": "csv",
+        "case_id_col": "CustomerID",
+        "timestamp_col": "ContactDate",
+        "activity_col": "ComplaintTopic_EN",
+        "sep": ";",
+    },
+}
+
+DEFAULT_PARAMS_CSV = {
+    "case_id_col": "case:concept:name",
+    "activity_col": "concept:name",
+    "timestamp_col": "time:timestamp",
+    "sep": ",",
+}
+
+DEFAULT_PARAMS_XES = {"attribute": "concept:name"}
