a9bb1bad1df85ca001bee48cf578dd241a1e9914
Paul <paul.hofbauer3@gmail.com>
2025-10-28 16:28:36 +0100

    commit to move machines

--- experiments/simulation/models.py
+++ experiments/simulation/models.py
@@ -1,7 +1,9 @@
 # %%
 import random
-from pm4py.objects.petri_net.obj import PetriNet, Marking
-from pm4py.objects.petri_net.utils import petri_utils
+from pm4py.pm4py.objects.petri_net.obj import PetriNet, Marking
+from pm4py.pm4py.objects.petri_net.utils import petri_utils
+from typing import TypeAlias
+from enum import Enum
 
 
 def make_sequence(name, labels):
@@ -29,18 +31,51 @@ def make_sequence(name, labels):
     return net, im, fm
 
 
+Net: TypeAlias = tuple[PetriNet, Marking, Marking]
+
+
+class Composition(Enum):
+    XOR = 0
+    AND = 1
+    LOOP = 2
+
+    def compose(self, left: Net, right: Net) -> Net:
+        net = PetriNet(f"{left[0].name + right[0].name}_{self.value}")
+        match self.value:
+            case Composition.XOR:
+                places = [PetriNet.Place("p_xor")]
+
+            case Composition.AND:
+                places = [PetriNet.Place("p_and")]
+
+            case Composition.LOOP:
+                places = [PetriNet.Place("p_loop")]
+
+
 def random_block_structured(
-    num_blocks=3, xor_prob=0.3, and_prob=0.3, loop_prob=0.1
+    num_blocks=3, xor_prob=0.3, and_prob=0.3, loop_prob=0.1, max_depth=3
 ):
     # TODO: Compose blocks recursively
     # Start with simple sequential composition for baseline
-    labels = [f"a{i}" for i in range(num_blocks)]
-    return make_sequence("seq_model", labels)
+    if max_depth == 0:
+        labels = [f"a{i}" for i in range(num_blocks)]
+        return make_sequence("seq_model", labels)
+    comp_op = random.choice(
+        [e.value for e in Composition], [xor_prob, and_prob, loop_prob]
+    )
+
+    left = random_block_structured(
+        num_blocks, xor_prob, and_prob, loop_prob, max_depth - 1
+    )
+    right = random_block_structured(
+        num_blocks, xor_prob, and_prob, loop_prob, max_depth - 1
+    )
+    return Composition(comp_op).compose(left, right)
 
 
 if __name__ == "__main__":
     import pm4py
 
-    pn, im, fm = random_block_structured(num_blocks=5)
+    pn, im, fm = random_block_structured(num_blocks=5, max_depth=2)
     pm4py.view_petri_net(pn, im, fm)
 # %%
