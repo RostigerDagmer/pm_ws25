ae7f0b11c1b5bf9eef793d866b9b40c24703b371
Paul <paul.hofbauer3@gmail.com>
2025-11-06 17:57:46 +0100

    add basic run dataset

--- experiments/simulation/noise.py
+++ experiments/simulation/noise.py
@@ -1,15 +1,16 @@
 import random
 from copy import deepcopy
-from pm4py.objects.log.obj import EventLog, Trace
+from typing import Optional
+from pm4py.pm4py.objects.log.obj import EventLog, Trace
 
 
 def inject_noise(
     log: EventLog,
-    p_insert=0.05,
-    p_delete=0.05,
-    p_swap=0.02,
-    labels=None,
-    activity_key="concept:name",
+    p_insert: float = 0.05,
+    p_delete: float = 0.05,
+    p_swap: float = 0.02,
+    labels: Optional[str] = None,
+    activity_key: str = "concept:name",
 ) -> EventLog:
     """
     Returns a *deepcopy* of the log with noise injected.
@@ -18,34 +19,46 @@ def inject_noise(
     noisy_log = EventLog(attributes=getattr(log, "attributes", {}))
 
     for trace in log:
-        new_events = list(deepcopy(trace))  # mutable copy of events list
-        i = 0
-        while i < len(new_events):
-            r = random.random()
-            if r < p_delete:
-                # delete this event
-                new_events.pop(i)
-                continue
-            elif r < p_delete + p_insert and labels:
-                # insert a new event copy with random label
-                new_event = deepcopy(new_events[i]) if new_events else {}
-                new_event[activity_key] = random.choice(labels)
-                new_events.insert(i, new_event)
-            elif r < p_delete + p_insert + p_swap and i < len(new_events) - 1:
-                # swap two adjacent events
-                new_events[i], new_events[i + 1] = (
-                    new_events[i + 1],
-                    new_events[i],
-                )
-            i += 1
-
         # rewrap into a PM4Py Trace object
-        new_trace = Trace(
-            new_events, attributes=getattr(trace, "attributes", {})
+        new_trace = inject_noise_trace(
+            trace, p_insert, p_delete, p_swap, labels, activity_key
         )
         noisy_log.append(new_trace)
 
     return noisy_log
+
+
+def inject_noise_trace(
+    trace: Trace,
+    p_insert: float = 0.05,
+    p_delete: float = 0.05,
+    p_swap: float = 0.02,
+    labels: Optional[str] = None,
+    activity_key: str = "concept:name",
+) -> Trace:
+    new_events = list(deepcopy(trace))  # mutable copy of events list
+    i = 0
+    while i < len(new_events):
+        r = random.random()
+        if r < p_delete:
+            # delete this event
+            new_events.pop(i)
+            continue
+        elif r < p_delete + p_insert and labels:
+            # insert a new event copy with random label
+            new_event = deepcopy(new_events[i]) if new_events else {}
+            new_event[activity_key] = random.choice(labels)
+            new_events.insert(i, new_event)
+        elif r < p_delete + p_insert + p_swap and i < len(new_events) - 1:
+            # swap two adjacent events
+            new_events[i], new_events[i + 1] = (
+                new_events[i + 1],
+                new_events[i],
+            )
+        i += 1
+
+    # rewrap into a PM4Py Trace object
+    return Trace(new_events, attributes=getattr(trace, "attributes", {}))
 
 
 """
