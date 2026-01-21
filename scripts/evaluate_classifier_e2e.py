6db71203eac5b7aefbbb1563b0b271e9f1cc48c7
Dagmer <paul.hofbauer3@gmail.com>
2026-01-21 14:40:03 +0100

    skip init for pm datasets

--- scripts/evaluate_classifier_e2e.py
+++ scripts/evaluate_classifier_e2e.py
@@ -47,38 +47,38 @@ OUTPUT_DIR = (
 TRAIN_DATASETS = {
     # 'd9769f3d-0ab0-4fb8-803b-0d1120ffcf54': ['Hospital_log.xes'],
     # '63a8435a-077d-4ece-97cd-2c76d394d99c': ['BPIC15_2.xes'],
-    # 'ed445cdd-27d5-4d77-a1f7-59fe7360cfbe': ['BPIC15_3.xes'],
-    # '679b11cf-47cd-459e-a6de-9ca614e25985': ['BPIC15_4.xes'],
-    # '3301445f-95e8-4ff0-98a4-901f1f204972': ['BPI%20Challenge%202018.xes'],
-    # '3926db30-f712-4394-aebc-75976070e91f': ['BPI_Challenge_2012.xes'],
-    # '6af6d5f0-f44c-49be-aac8-8eaa5fe4f6fd': [
-    #     'Hospital%20Billing%20-%20Event%20Log.xes'
-    # ],
-    # 'd06aff4b-79f0-45e6-8ec8-e19730c248f1': ['BPI_Challenge_2019.xes'],
-    # '3537c19d-6c64-4b1d-815d-915ab0e479da': [
-    #     'BPI_Challenge_2013_open_problems.xes'
-    # ],
-    # 'a0addfda-2044-4541-a450-fdcc9fe16d17': ['BPIC15_1.xes'],
-    # 'a6f651a7-5ce0-4bc6-8be1-a7747effa1cc': ['RequestForPayment.xes'],
-    # '500573e6-accc-4b0c-9576-aa5468b10cee': [
-    #     'BPI_Challenge_2013_incidents.xes'
-    # ],
+    'ed445cdd-27d5-4d77-a1f7-59fe7360cfbe': ['BPIC15_3.xes'],
+    '679b11cf-47cd-459e-a6de-9ca614e25985': ['BPIC15_4.xes'],
+    '3301445f-95e8-4ff0-98a4-901f1f204972': ['BPI%20Challenge%202018.xes'],
+    '3926db30-f712-4394-aebc-75976070e91f': ['BPI_Challenge_2012.xes'],
+    '6af6d5f0-f44c-49be-aac8-8eaa5fe4f6fd': [
+        'Hospital%20Billing%20-%20Event%20Log.xes'
+    ],
+    'd06aff4b-79f0-45e6-8ec8-e19730c248f1': ['BPI_Challenge_2019.xes'],
+    '3537c19d-6c64-4b1d-815d-915ab0e479da': [
+        'BPI_Challenge_2013_open_problems.xes'
+    ],
+    'a0addfda-2044-4541-a450-fdcc9fe16d17': ['BPIC15_1.xes'],
+    'a6f651a7-5ce0-4bc6-8be1-a7747effa1cc': ['RequestForPayment.xes'],
+    '500573e6-accc-4b0c-9576-aa5468b10cee': [
+        'BPI_Challenge_2013_incidents.xes'
+    ],
     '91fd1fa8-4df4-4b1a-9a3f-0116c412378f': ['InternationalDeclarations.xes'],
     'fb84cf2d-166f-4de2-87be-62ee317077e5': ['PrepaidTravelCost.xes'],
-    # '12683249': ['Road_Traffic_Fine_Management_Process.xes'],
-    # '33632f3c-5c48-40cf-8d8f-2db57f5a6ce7': [
-    #     'Sepsis%20Cases%20-%20Event%20Log.xes'
-    # ],
+    '12683249': ['Road_Traffic_Fine_Management_Process.xes'],
+    '33632f3c-5c48-40cf-8d8f-2db57f5a6ce7': [
+        'Sepsis%20Cases%20-%20Event%20Log.xes'
+    ],
 }
 
 TEST_DATASETS = {
     # 'b32c6fe5-f212-4286-9774-58dd53511cf8': ['BPIC15_5.xes'],
-    # '5f3067df-f10b-45da-b98b-86ae4c7a310b': ['BPI%20Challenge%202017.xes'],
+    '5f3067df-f10b-45da-b98b-86ae4c7a310b': ['BPI%20Challenge%202017.xes'],
     'db35afac-2133-40f3-a565-2dc77a9329a3': ['PermitLog.xes'],
     '6a0a26d2-82d0-4018-b1cd-89afb0e8627f': ['DomesticDeclarations.xes'],
-    # 'c2c3b154-ab26-4b31-a0e8-8f2350ddac11': [
-    #     'BPI_Challenge_2013_closed_problems.xes'
-    # ],
+    'c2c3b154-ab26-4b31-a0e8-8f2350ddac11': [
+        'BPI_Challenge_2013_closed_problems.xes'
+    ],
 }
 
 
@@ -107,7 +107,7 @@ if __name__ == "__main__":
 
     if args.use_tables:
         train_tables, test_tables, eval_tables = find_existing_tables(
-            Path(cache_path)
+            Path(cache_path), list(TRAIN_DATASETS.keys())
         )
     else:
         train_run_datasets = []
@@ -120,6 +120,7 @@ if __name__ == "__main__":
                     cache_path,
                     seed=SEED,
                     num_workers=16,
+                    skip_init=True,
                 )
                 if run_dataset is not None:
                     train_run_datasets.append(run_dataset)
@@ -210,6 +211,7 @@ if __name__ == "__main__":
                 cache_path,
                 seed=SEED,
                 num_workers=16,
+                skip_init=True,
             )
             test_run_datasets.append(run_dataset)
 
